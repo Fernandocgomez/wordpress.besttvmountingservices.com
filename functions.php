6154ca7c4b16742c125fe8f9f3eb7c9ee02face3
fernandocgomez <fernandocgomez@live.com>
2021-02-03 00:32:14 -0600

    keep only one style sheet

--- functions.php
+++ functions.php
@@ -12,19 +12,8 @@ add_action( 'wp_enqueue_scripts', 'load_theme_child_scripts_styles', 9999 );
 
 function load_theme_child_scripts_styles() {
     wp_enqueue_style("child-css", get_stylesheet_directory_uri()."/assets/css/main.css");
-    wp_enqueue_style("mobile-css", get_stylesheet_directory_uri()."/assets/css/mobile.css");
     wp_enqueue_script("child-js", get_stylesheet_directory_uri()."/assets/js/main.js");
 }
 
 //** Disable Gutenberg and Keep the Classic Editor */
 add_filter('use_block_editor_for_post', '__return_false', 10);
-
-//** Add media attribute for devices with a max-width of 600px */
-add_filter('style_loader_tag', 'add_media_attribute_mobile', 10, 2);
-
-function add_media_attribute_mobile($html, $handle) {
-    if( $handle === 'mobile-css' ) {
-        return str_replace( 'media="all"', 'media="screen and (max-width: 600px)"', $html );
-    }
-    return $html;
-}
